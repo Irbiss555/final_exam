e1221033ed137039a0d68a43ec8e223dfe3c0448
irbiss <irnazarov.b.i@gmail.com>
2022-07-09 11:58:21 +0600

    #3 Создал модель категорий

--- publications/models.py
+++ publications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+class Publication(models.Model):
