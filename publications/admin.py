c4519e22c9a3bf84e42b2accbdd719e3a8044cae
irbiss <irnazarov.b.i@gmail.com>
2022-07-09 12:42:17 +0600

    #3 Зарегистрировал модели в админке

--- publications/admin.py
+++ publications/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from publications.models import Publication, Category
+
+admin.site.register(Publication)
+admin.site.register(Category)
