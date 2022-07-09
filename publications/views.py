82c2d7e0538dc62ca7792f00b0220dd3fbbae4b8
irbiss <irnazarov.b.i@gmail.com>
2022-07-09 14:46:48 +0600

    #4 Создал представление для создания объявления

--- publications/views.py
+++ publications/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-from django.views.generic import ListView
+from django.urls import reverse
+from django.views.generic import ListView, CreateView
 
+from publications.forms import PublicationForm
 from publications.models import Publication
-from publications.services import publication_list_service
+from publications.services import publication_list_service, publication_create_service
 
 
 class PublicationListView(ListView):
@@ -14,3 +16,22 @@ class PublicationListView(ListView):
 
     def get_queryset(self):
         return publication_list_service()
+
+
+class PublicationCreateView(CreateView):
+    template_name = 'publications/publication_create.html'
+    model = Publication
+    context_object_name = 'publication'
+    form_class = PublicationForm
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form = self.get_form()
+        if form.is_valid():
+            publication_create_service(user=self.request.user, **form.cleaned_data)
+            return redirect(self.get_success_url())
+        else:
+            return self.form_invalid(form)
+
+    def get_success_url(self):
+        return reverse('accounts:user_detail', kwargs={'pk': self.request.user.pk})
