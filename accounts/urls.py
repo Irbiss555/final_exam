8b91741f4c79653b901d27197dcc9c367daa25c7
irbiss <irnazarov.b.i@gmail.com>
2022-07-09 11:38:32 +0600

    #2 Добавил роут для личного кабинета пользователя

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='user_create'),
+    path('user/<int:pk>', UserDetailView.as_view(), name='user_detail'),
 ]
