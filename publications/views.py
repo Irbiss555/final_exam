4b2cdeac796966e5130f8f03f0279437b8faf4ae
irbiss <irnazarov.b.i@gmail.com>
2022-07-09 13:15:15 +0600

    #3 Создал представление для списка объявлений

--- publications/views.py
+++ publications/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from publications.models import Publication
+from publications.services import publication_list_service
+
+
+class PublicationListView(ListView):
+    template_name = 'publications/publication_list.html'
+    model = Publication
+    context_object_name = 'publications'
+
+    def get_queryset(self):
+        return publication_list_service()
