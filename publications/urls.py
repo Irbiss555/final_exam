f1b0614a6d727ce301dcc5e720a1f01a92694ed7
irbiss <irnazarov.b.i@gmail.com>
2022-07-09 14:46:59 +0600

    #4 Добавил роут для создания объявления

--- publications/urls.py
+++ publications/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from core import settings
-from publications.views import PublicationListView
+from publications import views
 
 app_name = 'publications'
 
 urlpatterns = [
-    path('', PublicationListView.as_view(), name='publication_list'),
+    path('', views.PublicationListView.as_view(), name='publication_list'),
+    path('create/', views.PublicationCreateView.as_view(), name='publication_create'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
